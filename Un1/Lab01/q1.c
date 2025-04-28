532506282da23e6671dd35080152b57e427cedcb
Joaof14 <joao-pedro032011@hotmail.com>
2025-04-28 17:38:22 -0300

    questão1lab01Un1

---
+++ Un1/Lab01/q1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    char comidaFavorita[50];
+    printf("Digite sua comida favorita \n");
+    scanf("%s", comidaFavorita);
+    printf("Sua comida favorita é %s", comidaFavorita);
+    return 0;
+}
