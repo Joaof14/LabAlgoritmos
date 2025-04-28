092657de1be2ffeed6d946bc6bed1ab7f46cf5cc
Joaof14 <joao-pedro032011@hotmail.com>
2025-04-28 18:28:52 -0300

    'nao finalizado'

---
+++ Un1/Lab01/q9.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+
+// Peça para o usuário digitar um símbolo especial, um número e uma letra. Exiba uma "senha" com essa combinação. 
+
+int main() {
+    char letra, simbolo;
+    int numero;
+    
+
+
+    return 0;
+}
