2a4d586e41153b9bee3f70757211fcc3167a0b9d
Joaof14 <joao-pedro032011@hotmail.com>
2025-04-28 15:42:51 -0300

    "q8"

--- Un1/Lab01/q8.c
+++ Un1/Lab01/q8.c
@@ -3,9 +3,9 @@
 //  Leia um caractere qualquer (como um símbolo: @, #, %, &, etc.) e exiba ele entre colchetes. 
 
 int main (){
-    char c;
+    char caracter;
     printf("Digite um símbolo (@, #, %%, &, etc.) e exiba ele\n");
-    scanf("%s", &c);
-    printf("[%c]", &c);
+    scanf("%s", &caracter);
+    printf("[%s]\n", &caracter);
     return 0;
 }
