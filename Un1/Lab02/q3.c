d9bf1f09e0d774cc10067d8399e9d50c3501a364
Joaof14 <joao-pedro032011@hotmail.com>
2025-05-04 16:52:55 -0300

    ""

--- Un1/Lab02/q3.c
+++ Un1/Lab02/q3.c
@@ -2,3 +2,28 @@
 
 /* Crie um programa que verifique se um número fornecido pelo usuário é
 primo. */
+
+int main(){
+    int numero, primo = 1;
+    //Recebimento do valor
+    printf("Digite um número para que possamos descobrir se ele é primo ou não\n");
+    scanf("%d", &numero);
+
+
+    //Verificação
+    for (int i = 2; i < numero; i++){
+        if (numero % i == 0) 
+        {
+            primo = 0;
+            break;
+        }
+    }
+    if (primo == 0){
+        printf("O número %d não é primo\n", numero);
+    }
+    else{
+        printf("O número %d é primo\n", numero);
+    }
+
+    return 0;
+}
